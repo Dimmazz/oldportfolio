1f63186b0cc04d57c2a5c8975530c03696e4c031
Mazu <zaskok20@yandex.ru>
2021-09-16 18:37:17 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(function() {
+
+      // Главный слайдер с контентом
+  $('.skills__slider').slick({
+    Infinity: true,
+    dots: false,
+    arrows: false,
+    asNavFor: '.skills__dots',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          dots: true,
+        }
+      }]
+    })
+
+      // Доп слайдер для главного 
+  $('.skills__dots').slick({
+    Infinity: true,
+    arrows: false,
+    dots: false,
+    slidesToShow: 6,
+    slidesToScroll: 6,
+    // centerMode: true,
+    asNavFor: '.skills__slider'
+  })
+
+      // Форма отправки обратной связи
+  
+
+})
